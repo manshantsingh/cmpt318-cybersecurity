94870f69a76ba3fc077c1add051f4c83b6f0cbf8
Manshant Singh Kohli <me@manshantsingh.com>
2018-03-28 23:25:19 -0700

    added (broken) multivariate hmm

--- multivariate_hmm.py
+++ multivariate_hmm.py
@@ -6,13 +6,22 @@ from hmmlearn import hmm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 
+import warnings
+# warnings.filterwarnings(action='once')
+warnings.filterwarnings(action='ignore')
+
+# change me
+columns_to_use = ['Global_active_power', 'Global_reactive_power', 'Voltage',
+			       'Global_intensity', 'Sub_metering_1', 'Sub_metering_2',
+			       'Sub_metering_3']
+# columns_to_use = ['Global_active_power', 'Voltage']
 
 with open('train-friday_night.pickle', 'rb') as handle:
     x = pickle.load(handle)
 
 arr = []
 for a in x:
-	v = a.set_index(['d']).Global_active_power.values
+	v = a.set_index(['d'])[columns_to_use].dropna().values
 	if len(v) == 1440:
 		arr.append(v[1080:])
 
@@ -21,20 +30,25 @@ arr = np.array(arr, dtype=np.float64)
 def score(m, a):
 	val = []
 	for v in a:
-		val.append(m.score([v]))
+		val.append(m.score(v))
 	val = np.array(val)
 	return [val.mean(), val.std(), val.min(), val.max()]
 
 
-def wow(n,f=5,nn=50):
-	k = KFold(f)
+def wow(n_components, n_folds=5, n_iter=50):
+	k = KFold(n_folds)
+
 	for a_ind, b_ind in k.split(arr):
 		a = arr[a_ind]
 		b = arr[b_ind]
-		m = hmm.GaussianHMM(n_components=n, n_iter=nn)
-		m.fit(a)
+		m = hmm.GaussianHMM(n_components=n_components, n_iter=n_iter)
+
+		# reshaping into the required format
+		m.fit(a.reshape(-1, len(columns_to_use)), np.full(len(a), len(a[0])))
+
 		a = score(m,a)
 		b = score(m,b)
+
 		print("\n\n train vs test")
 		print("mean:    ", a[0], " vs ", b[0])
 		print("std:     ", a[1], " vs ", b[1])
