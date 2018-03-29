5228fbc8d05c31870b6e79ca447c3ff3398b224d
Manshant Singh Kohli <me@manshantsingh.com>
2018-03-28 23:50:02 -0700

    code cleanup

--- multivariate_hmm.py
+++ multivariate_hmm.py
@@ -14,7 +14,6 @@ warnings.filterwarnings(action='ignore')
 columns_to_use = ['Global_active_power', 'Global_reactive_power', 'Voltage',
 			       'Global_intensity', 'Sub_metering_1', 'Sub_metering_2',
 			       'Sub_metering_3']
-# columns_to_use = ['Global_active_power', 'Voltage']
 
 with open('train-friday_night.pickle', 'rb') as handle:
     x = pickle.load(handle)
@@ -34,22 +33,28 @@ def score(m, a):
 	val = np.array(val)
 	return [val.mean(), val.std(), val.min(), val.max()]
 
-
 def wow(n_components, n_folds=5, n_iter=50):
 	k = KFold(n_folds)
 
 	for a_ind, b_ind in k.split(arr):
-		a = arr[a_ind]
-		b = arr[b_ind]
+
+		# train
+		a_orig = arr[a_ind]
+		a_len = np.full(len(a_orig), len(a_orig[0]))
+		a = a_orig.reshape(-1, len(columns_to_use))
+
+		# test
+		b_orig = arr[b_ind]
+		b_len = np.full(len(b_orig), len(b_orig[0]))
+		b = b_orig.reshape(-1, len(columns_to_use))
+
+		# fitting the model
 		m = hmm.GaussianHMM(n_components=n_components, n_iter=n_iter)
+		m.fit(a, a_len)
 
-		# reshaping into the required format
-		m.fit(a.reshape(-1, len(columns_to_use)), np.full(len(a), len(a[0])))
-
-		a = score(m,a)
-		b = score(m,b)
-
-		print("\n\n train vs test")
+		a = score(m,a_orig)
+		b = score(m,b_orig)
+		print("train(",len(a_orig),") vs test(",len(b_orig),")")
 		print("mean:    ", a[0], " vs ", b[0])
 		print("std:     ", a[1], " vs ", b[1])
 		print("min:     ", a[2], " vs ", b[2])
